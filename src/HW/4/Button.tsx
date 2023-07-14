339662a5242502e6505599d2d5885a01769714b5
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2023-07-14 14:54:53 +0200

    add: homework 4 (new version of tasks)

---
+++ src/HW/4/Button.tsx
@@ -0,0 +1,16 @@
+type ButtonPropsType = {
+  callBack: ()=>void // НУЖНО ПРОТИПИЗИРОВАТЬ
+  name: string // НУЖНО ПРОТИПИЗИРОВАТЬ
+};
+
+export const Button = (props: ButtonPropsType) => {
+  const callBackHandler = () => {
+    props.callBack()
+  };
+
+  return (
+    <button id={'hw04-button'} onClick={callBackHandler}>
+      {props.name}
+    </button>
+  );
+};
---
+++ src/HW/4/Input.tsx
@@ -0,0 +1,16 @@
+import React,{ChangeEvent} from "react";
+
+type InputPropsType = {
+	currentText: string // НУЖНО ПРОТИПИЗИРОВАТЬ
+	setCurrentText: (currentText: string)=>void // НУЖНО ПРОТИПИЗИРОВАТЬ
+};
+
+export const Input = (props: InputPropsType) => {
+	const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+		props.setCurrentText(event.currentTarget.value)
+	};
+
+	return (
+	  <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
+	);
+};
