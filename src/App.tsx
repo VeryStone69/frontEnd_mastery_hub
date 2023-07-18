20505aa8b589d018493530c6c0187d4ae8a9cd7d
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2023-07-18 22:50:35 +0200

    add: task for split_2

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import './App.css';
-import { HW1 } from './HW/1/HW1';
-import { HW2 } from './HW/2/HW2';
-import { HW3 } from './HW/3/HW3';
-import { HW4 } from './HW/4/HW4';
+import {HW1} from './HW/1/HW1';
+import {HW2} from './HW/2/HW2';
+import {HW3} from './HW/3/HW3';
+import {HW4} from './HW/4/HW4';
 
 function App() {
-  return (
-    <div className="App">
-      <HW1 />
-      <HW2 />
-      <HW3 />
-      <HW4 />
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* ===== Tasks for Split 1 ===== */}
+            {/*<HW1 />*/}
+            {/*<HW2 />*/}
+            {/*<HW3 />*/}
+            {/*<HW4 />*/}
+            {/* ===== Tasks for Split 2 ===== */}
+
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';         // ===== Tasks for Split 1 =====
+import App from "./s1-main/App";    // ===== Tasks for Split 2 =====
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
