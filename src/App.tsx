f15429cd59b446bf931d282dee35bac477d4f033
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2023-07-18 11:04:43 +0200

    add: HomeWork 2 (NEW Version/Split 1)

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import { HW4 } from './HW/4/HW4';
 function App() {
   return (
     <div className="App">
-      <HW1 />
-      {/*<HW2 />*/}
+      {/*<HW1 />*/}
+      <HW2 />
       {/*<HW3 />*/}
       {/*<HW4 />*/}
     </div>
