ca39d3c6f633c571005fccc9486d8d478acd2168
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2023-09-07 13:22:06 +0200

    update: homework 8 (filter, reduser, style)

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,21 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
-        case 'sort': { // by name
+        case 'sort': {
+            const sortBy = (a: UserType, b: UserType) => {
+                const nameA = a.name;
+                const nameB = b.name;
+                return action.payload === "up" ? nameA.localeCompare(nameB) : nameB.localeCompare(nameA);
+            };
 
-            return state // need to fix
+            return [...state].sort(sortBy);
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(el => el.age > 18);
         }
         default:
-            return state
+            return state;
     }
-}
+};
