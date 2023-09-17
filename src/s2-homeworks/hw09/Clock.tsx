d808090e97be23ec47a28a0fa2c7b95d5722a136
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2023-09-17 14:43:33 +0200

    change: homework #9 - date format

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -35,7 +35,7 @@ function Clock() {
     const zeroForStringTime = (num: number) => num < 10 ? "0" + num : num
     const stringTime = `${zeroForStringTime(date.getHours())}:${zeroForStringTime(date.getMinutes())}:${zeroForStringTime(date.getSeconds())}` ||
         <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date.toLocaleDateString('en-US', {year: 'numeric', month: '2-digit', day: '2-digit'}).replace(/\//g, '.') || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = date.toLocaleDateString('ru-RU', {year: 'numeric', month: '2-digit', day: '2-digit'}).replace(/\//g, '.') || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = date.toLocaleDateString('en-US', {weekday: "long"}) || <br/> // пишут студенты
