e791cb8082eb648740020cd572de9d6ed1cad885
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2023-10-25 20:56:34 +0200

    add: homework #11 - slider, double slider, mui

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -12,12 +12,22 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 
 function HW11() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
-    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 20))
+    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 80))
 
-    const change = (event: any, value: any) => {
-        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-    }
+    const change = (event: Event, value: number | number[]) => {
+            if (Array.isArray(value)){
+                setValue1(value[0])
+                setValue2(value[1])
+            }
+            else {
+                setValue1(value)
+            }
+
+        }
+
+        //пишет студент  если пришёл массив - сохранить значения в оба useState, иначе в первый
+
 
     return (
         <div id={'hw11'}>
@@ -26,21 +36,24 @@ function HW11() {
             <div className={s2.hw}>
                 <div className={s.container}>
                     <div className={s.wrapper}>
-                        <span id={'hw11-value'} className={s.number}>{value1}</span>
+                        <span id={'hw11-value'} className={s.numberLeft}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
-
+                            value={value1}
+                            callback = {change}
                         />
                     </div>
                     <div className={s.wrapper}>
-                        <span id={'hw11-value-1'} className={s.number}>{value1}</span>
+                        <span id={'hw11-value-1'} className={s.numberLeft}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
+                            value={[value1,value2]}
+                            callback = {change}
                             // сделать так чтоб value1/2 изменялось // пишет студент
 
                         />
-                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+                        <span id={'hw11-value-2'} className={s.numberRight}>{value2}</span>
                     </div>
                 </div>
             </div>
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
 
-const SuperRange: React.FC<SliderProps> = (props) => {
+type PropsType = {
+    callback: (event: Event, value: number | number[]) => void
+    id: string
+}
+
+const SuperRange: React.FC<SliderProps & PropsType> = (props) => {
+    const onChangeHandler = (event: Event, value: number | number[]) => {
+        props.callback(event, value)
+    }
     return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
+        <>
+            <Slider
+                sx={{ // стили для слайдера // пишет студент
+                    width: 550,
+                }}
+                onChange={onChangeHandler}
+                {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+            />
+
+        </>
     )
 }
 
